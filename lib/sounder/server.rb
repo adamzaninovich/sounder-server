fc8044c41ed04096eb8d4b644b510ef3ce872ef3
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-08-06 18:43:57 -0700

    a modest beginning

--- lib/sounder/server.rb
+++ lib/sounder/server.rb
@@ -4,8 +4,33 @@ require "sounder/server/version"
 
 module Sounder
   class Server < Sinatra::Base
+    @@soundpacks = {}
+    def self.soundpacks
+      @@soundpacks
+    end
+    def self.register_soundpack name
+      @@soundpacks[name] = 'something'
+    end
+
     get '/' do
       'This will eventually list all the sounds'
+    end
+
+    get '/system/volume/:level' do |level|
+      Sounder::System.set_volume(level)
+      "Set volume to #{level}"
+    end
+
+    get '/soundpack/:name' do |name|
+      "#{name}: #{@@soundpacks[name.to_sym]}"
+    end
+
+    get '/sound/:name' do |name|
+      "#{name}: #{get_sound(name)}"
+    end
+
+    get '/sound/:name/play' do |name|
+      self.get_sound(name).play
     end
   end
 end
--- spec/sounder/server_spec.rb
+++ spec/sounder/server_spec.rb
@@ -11,4 +11,16 @@ describe Sounder::Server do
     last_response.should be_ok
     last_response.body.should == 'This will eventually list all the sounds'
   end
+
+  describe "Soundpack Registration" do
+    before do
+      Sounder::Server.register_soundpack :archer
+    end
+
+    it 'adds the soundpack to Sounder::Server.soundpacks' do
+      Sounder::Server.soundpacks.should have_key :archer
+    end
+
+    it 'actually adds the sound'
+  end
 end
